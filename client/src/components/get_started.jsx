2de4eadd64e3519434a19ce8792bd2b3201610a4
Elly <elly@mg.crane.ai>
2018-10-30 20:20:54 -0400

    added client/src/components/get_started refactoring

---
+++ client/src/components/get_started.jsx
@@ -0,0 +1,86 @@
+
+/*
+get started view
+created by: FusionCode
+
+//----------------------------------------------------------------------------------------------------------
+// Component Requirements
+//----------------------------------------------------------------------------------------------------------
+
+get started view has Label located at the top left of the view, text needs identified
+
+*/
+        
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Row, Col , Button } from 'reactstrap';
+        
+        
+        import {  }  from '../actions/get_startedActions.js'; 
+
+        
+        
+       
+        class getstartedView extends React.Component {
+
+            state = {
+                
+                
+            }
+    
+                
+            
+
+            componentDidMount = () => {
+                
+               
+                
+            }
+    
+        render() {
+        
+        
+        const { data } = this.props
+
+            return (
+                <Row>
+
+                 
+                    <Col md={12}>
+
+                        				{/*<!-- TODO: Confirm label text --> */}
+
+			</Col>
+
+			<Col md={12}>
+				{/* <!-- TODO: Create unit test for get_started action --> */}
+				<Button 
+                        className="btn btn-lg btn-block " 
+                        onClick={() => this.get_started()}>get started</Button> 
+
+			</Col>
+
+
+                           
+   
+                    </Col> 
+                     
+                </Row>
+            )   
+        }
+    }
+                   
+    function mapStateToProps(state) {
+            
+            }
+        }
+    
+        function mapDispatchToProps(dispatch) {
+            return bindActionCreators({   }, dispatch);
+        }
+    
+   
+        export default connect(mapStateToProps, mapDispatchToProps)(getstartedView);
+    
+        
